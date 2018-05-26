3925626a2419bd6552ea979bd57adf1761cb9761
jemsley canela <jemscanela@gmail.com>
2018-05-26 01:34:50 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ var playlist = new Object({
 })
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist[songTitle]=artistName;
+  playlist[artistName]=songTitle;
   return playlist
 }
 
